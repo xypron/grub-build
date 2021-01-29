377f6af785e96ef65c9b00b4bc0cda1b85ca1cb3
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-01-29 18:17:41 +0100

    0001-efi-EFI-Device-Tree-Fixup-Protocol.patch
    Just the implementation part of the patch
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-grub.sh
+++ patch/patch-grub.sh
@@ -5,3 +5,4 @@ git am --abort || true
 
 git am ../patch/0001-efi-device-tree-must-be-in-EfiACPIReclaimMemory.patch
 git am ../patch/0001-lsefisystab-short-text-for-EFI_RT_PROPERTIES_TABLE_G.patch
+git am ../patch/0001-efi-EFI-Device-Tree-Fixup-Protocol.patch
