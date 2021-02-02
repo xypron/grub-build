2917b2132548b2b1928a96d339adad87de7e900b
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-02-02 23:40:14 +0100

    patch-grub.sh: sequence
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-grub.sh
+++ patch/patch-grub.sh
@@ -5,5 +5,5 @@ git am --abort || true
 
 git am ../patch/0001-efi-device-tree-must-be-in-EfiACPIReclaimMemory.patch
 git am ../patch/0001-lsefisystab-short-text-for-EFI_RT_PROPERTIES_TABLE_G.patch
-git am ../patch/0001-10_linux-support-loading-device-trees.patch
 git am ../patch/0001-efi-EFI-Device-Tree-Fixup-Protocol.patch
+git am ../patch/0001-10_linux-support-loading-device-trees.patch
