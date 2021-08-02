5a6d402a3ced395dd6f9c353dd309b7f31cab544
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-08-02 12:58:50 +0200

    RISC-V patches
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- patch/patch-grub.sh
+++ patch/patch-grub.sh
@@ -7,3 +7,11 @@ git am ../patch/0001-efi-device-tree-must-be-in-EfiACPIReclaimMemory.patch
 git am ../patch/0001-lsefisystab-short-text-for-EFI_RT_PROPERTIES_TABLE_G.patch
 git am ../patch/0001-efi-EFI-Device-Tree-Fixup-Protocol.patch
 git am ../patch/0001-10_linux-support-loading-device-trees.patch
+
+git am ../patch/0001-loader-drop-argv-argument-in-grub_initrd_load.patch
+git am ../patch/0002-efi-add-definition-of-LoadFile2-protocol.patch
+git am ../patch/0003-efi-implemented-LoadFile2-initrd-loading-protocol-fo.patch
+git am ../patch/0004-linux-ignore-FDT-unless-we-need-to-modify-it.patch
+git am ../patch/0005-loader-Move-arm64-linux-loader-to-common-code.patch
+git am ../patch/0006-RISC-V-Update-image-header.patch
+git am ../patch/0007-RISC-V-Use-common-linux-loader.patch
