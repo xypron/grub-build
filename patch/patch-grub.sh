ac15349b9095f524b53249505c1d942489f3ef6c
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2021-08-13 17:21:53 +0200

    0001-efi-EFI-Device-Tree-Fixup-Protocol.patch
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- patch/patch-grub.sh
+++ patch/patch-grub.sh
@@ -3,9 +3,6 @@ set -e
 
 git am --abort || true
 
-git am ../patch/0001-efi-EFI-Device-Tree-Fixup-Protocol.patch
-git am ../patch/0001-10_linux-support-loading-device-trees.patch
-
 git am ../patch/0001-libgcrypt-avoid-Wsign-compare-in-rijndael-do_setkey.patch
 git am ../patch/0001-libgcrypt-avoid-Wempty-body-in-rijndael-do_setkey.patch
 git am ../patch/0001-mpi-avoid-Wunused-but-set-variable-in-UDIV_QRNND_PRE.patch
@@ -19,3 +16,5 @@ git am ../patch/0001-loader-Move-arm64-linux-loader-to-common-code.patch
 git am ../patch/0001-RISC-V-Update-image-header.patch
 git am ../patch/0001-RISC-V-Use-common-linux-loader.patch
 
+git am ../patch/0001-efi-EFI-Device-Tree-Fixup-Protocol.patch
+git am ../patch/0001-10_linux-support-loading-device-trees.patch
