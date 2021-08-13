1329af0b5cb432311303952e1aeb6b8b0e38be1e
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2021-08-13 15:17:59 +0200

    0001-mpi-avoid-Wunused-but-set-variable-in-_gcry_mpih_div.patch
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- patch/patch-grub.sh
+++ patch/patch-grub.sh
@@ -8,6 +8,8 @@ git am ../patch/0001-10_linux-support-loading-device-trees.patch
 
 git am ../patch/0001-libgcrypt-avoid-Wsign-compare-in-rijndael-do_setkey.patch
 git am ../patch/0001-libgcrypt-avoid-Wempty-body-in-rijndael-do_setkey.patch
+git am ../patch/0001-mpi-avoid-Wunused-but-set-variable-in-UDIV_QRNND_PRE.patch
+git am ../patch/0001-mpi-avoid-Wunused-but-set-variable-in-_gcry_mpih_div.patch
 
 git am ../patch/0001-loader-drop-argv-argument-in-grub_initrd_load.patch
 git am ../patch/0001-efi-add-definition-of-LoadFile2-protocol.patch
