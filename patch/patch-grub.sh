b01ff4a07ff066e9bd8ceb348b0d6738909e531a
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2021-08-13 13:48:29 +0200

    Support booting RISC-V Linux
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- patch/patch-grub.sh
+++ patch/patch-grub.sh
@@ -9,9 +9,9 @@ git am ../patch/0001-efi-EFI-Device-Tree-Fixup-Protocol.patch
 git am ../patch/0001-10_linux-support-loading-device-trees.patch
 
 git am ../patch/0001-loader-drop-argv-argument-in-grub_initrd_load.patch
-git am ../patch/0002-efi-add-definition-of-LoadFile2-protocol.patch
-git am ../patch/0003-efi-implemented-LoadFile2-initrd-loading-protocol-fo.patch
-git am ../patch/0004-linux-ignore-FDT-unless-we-need-to-modify-it.patch
-git am ../patch/0005-loader-Move-arm64-linux-loader-to-common-code.patch
-git am ../patch/0006-RISC-V-Update-image-header.patch
-git am ../patch/0007-RISC-V-Use-common-linux-loader.patch
+git am ../patch/0001-efi-add-definition-of-LoadFile2-protocol.patch
+git am ../patch/0001-efi-implemented-LoadFile2-initrd-loading-protocol-fo.patch
+git am ../patch/0001-linux-ignore-FDT-unless-we-need-to-modify-it.patch
+git am ../patch/0001-loader-Move-arm64-linux-loader-to-common-code.patch
+git am ../patch/0001-RISC-V-Update-image-header.patch
+git am ../patch/0001-RISC-V-Use-common-linux-loader.patch
