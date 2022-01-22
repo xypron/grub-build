54e2db59288f0a5352f5116f6af15ff560eb12d5
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-01-22 12:00:13 +0100

    0001-efi-implement-grub_efi_run_image.patch
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- patches/series.sh
+++ patches/series.sh
@@ -124,6 +124,6 @@ git am ../patches/0099-chainloader-Avoid-a-double-free-when-validation-fail.patc
 git am ../patches/0105-efilinux-Fix-integer-overflows-in-grub_cmd_initrd.patch
 git am ../patches/ubuntu-verifiers-last.patch
 
-git am ../patches/0001-efi-correct-struct-grub_efi_boot_services.patcht
+git am ../patches/0001-efi-correct-struct-grub_efi_boot_services.patch
 git am ../patches/0001-fat-fix-listing-the-root-directory.patch
 git am ../patches/0001-efi-implement-grub_efi_run_image.patch
