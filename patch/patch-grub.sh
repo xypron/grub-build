4a34a30951166afc8c9cbd3d1c0af076b00c5062
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-09-01 15:30:20 +0200

    Rebase patches
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- patch/patch-grub.sh
+++ patch/patch-grub.sh
@@ -3,10 +3,13 @@ set -e
 
 git am --abort || true
 
+git checkout build
+git reset --hard origin/master
+
 git am ../patch/0001-mpi-avoid-Wunused-but-set-variable-in-UDIV_QRNND_PRE.patch
 git am ../patch/0001-mpi-avoid-Wunused-but-set-variable-in-_gcry_mpih_div.patch
 
-git am ../patch/0001-loader-drop-argv-argument-in-grub_initrd_load.patch
+# git am ../patch/0001-loader-drop-argv-argument-in-grub_initrd_load.patch
 git am ../patch/0001-efi-add-definition-of-LoadFile2-protocol.patch
 git am ../patch/0001-efi-implemented-LoadFile2-initrd-loading-protocol-fo.patch
 git am ../patch/0001-linux-ignore-FDT-unless-we-need-to-modify-it.patch
