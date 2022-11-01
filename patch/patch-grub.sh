33f3ef5edc3c6940b874964ba2c3a26e52d246df
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-11-01 11:56:49 +0100

    Remove accepted patches
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- patch/patch-grub.sh
+++ patch/patch-grub.sh
@@ -9,14 +9,7 @@ git reset --hard origin/master
 git am ../patch/0001-mpi-avoid-Wunused-but-set-variable-in-UDIV_QRNND_PRE.patch
 git am ../patch/0001-mpi-avoid-Wunused-but-set-variable-in-_gcry_mpih_div.patch
 
-git am ../patch/0001-commands-efi-lsefisystab-Short-text-for-EFI_CONFORMA.patch
-
-git am ../patch/0001-efi-move-MS-DOS-stub-out-of-generic-PE-header-defini.patch
-
 # git am ../patch/0001-loader-drop-argv-argument-in-grub_initrd_load.patch
-git am ../patch/0001-efi-add-definition-of-LoadFile2-protocol.patch
-git am ../patch/0001-efi-implemented-LoadFile2-initrd-loading-protocol-fo.patch
-git am ../patch/0001-linux-ignore-FDT-unless-we-need-to-modify-it.patch
 git am ../patch/0001-loader-Move-arm64-linux-loader-to-common-code.patch
 git am ../patch/0001-RISC-V-Update-image-header.patch
 git am ../patch/0001-RISC-V-Use-common-linux-loader.patch
